2aef27bc1626a3ba30076b3070aaed45c31ea886
mahithhhshashankkk <162897517+mahithhhshashankkk@users.noreply.github.com>
2025-07-15 17:24:32 +0530

    Update application.js

--- backend/models/application.js
+++ backend/models/application.js
@@ -1,1 +1,35 @@
+const mongoose = require('mongoose');
 
+const applicationSchema = new mongoose.Schema({
+  job_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Job',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  resume_link: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  cover_letter: {
+    type: String,
+    required: true
+  },
+  appliedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Application', applicationSchema);
