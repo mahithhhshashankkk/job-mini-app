8f4b42f5551a76655dfa4dfe21db4b80bcd2b065
mahithhhshashankkk <162897517+mahithhhshashankkk@users.noreply.github.com>
2025-07-15 17:39:06 +0530

    Update app.tsx

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import JobDetail from './pages/JobDetail';
+import ApplicationForm from './pages/ApplicationForm';
+import Admin from './pages/Admin';
 
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs/:id" element={<JobDetail />} />
+        <Route path="/jobs/:id/apply" element={<ApplicationForm />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
